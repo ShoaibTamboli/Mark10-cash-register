a7d01a362fec75a7df8f4ccb878f8919cac9f614
ShoaibTamboli <shoaibtamboli03@gmail.com>
2021-09-29 00:50:22 +0530

    Update: Js added

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
-var billamount=document.querySelector("#bill-amount");
+var billamount = document.querySelector("#bill-amount");
 
-var cashamountrecvd=document.querySelector(".cash-amount");
+var cashamountrecvd = document.querySelector(".cash-amount");
 
- var btncheck= document.querySelector("#btn-check");
+var btncheck = document.querySelector("#btn-check");
 
- var message=document.querySelector(".error-msg")
+var message = document.querySelector(".error-msg")
 
- btncheck.addEventListener("click",function btnCheckClickListner(){
-     message.style.display="block"
-     if( billamount.value>cashamountrecvd.value || billamount.value===""){
-         console.log("InValid")
-         message.innerText="Enter Valid Amount"
-     }
-     else{
-        message.style.display="none"
-         console.log("valid")
-        
-         
-     }
- })
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+btncheck.addEventListener("click", function validateBillAndCashAmount() {
+  hideMessage();
+  if (billamount.value > 0) {
+    
+    if (cashamountrecvd.value >= billamount.value) {
+     
+      const amountToBeReturned = cashamountrecvd.value - billamount.value; 
+      calculateChange(amountToBeReturned);
+    } else {
+      showMessage("Do you wanna wash plates?");
+    }
+  } else {
+    showMessage("Invalid Bill Amount");
+  }
+});
+
+function calculateChange(amountToBeReturned) {
+  
+  for (let i = 0; i < availableNotes.length; i++) {
+   
+    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+   
+
+    
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
+   
+    
+    noOfNotes[i].innerText = numberOfNotes;
+  }
+}
+
+function hideMessage() {
+  message.style.display = "none";
+}
+
+function showMessage(msg) {
+  message.style.display = "block";
+  message.innerText = msg;
+}
